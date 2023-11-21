2ed37ba25ffc1b7a235931f877780fd4819373d3
Manishbhosale <manish.b@logicaldna.com>
2023-11-21 10:55:42 +0530

    add test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> hello
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    hello
+NameError: name 'hello' is not defined
+>>> print("Hello")
+Hello
+>>> 
